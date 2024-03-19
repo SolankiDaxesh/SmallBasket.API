367567f4befd33b15f6e0fc9a85d78a8fa372f5a
SolankiDaxesh <solankidaxesh912@gmail.com>
2024-03-19 09:20:17 +0530

    Users_RM created

--- Model/Users_RM.cs
+++ Model/Users_RM.cs
@@ -2,12 +2,11 @@
 {
     public class Users_RM
     {
-        public Guid UserId { get; set; }
+        public Guid? UserId { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string MobileNumber { get; set; }
         public string Password { get; set; }
-        public bool IsDelete { get; set; } = false;
-        public DateTime CreatedDate { get; set; }
+      
     }
 }
